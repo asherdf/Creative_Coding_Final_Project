f7d31b523935049aab2a6732104191426cdc1f0f
asherdf <asherdf5@gmail.com>
2017-12-11 14:05:02 -0500

    Text Displays
    Dec 11, 2017 14:04

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
+function preload(){
+	poem = loadStrings('Shel_Silverstein.txt');
+}
+
+//---------------------------------------------------------------
 function setup() { 
-	createCanvas(400,400);
+	createCanvas(1000,800);
+	//text(poem, width/2, height/2, 80, 80);
 }
 
 //---------------------------------------------------------------
 function draw() {
-	background(255);
+	background(0);
+	fill(255);
+	text(poem, 0,0, width, height);
 }
